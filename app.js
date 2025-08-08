49140505329b65d2f35f6d09bb0ab69909b6835c
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@CFP127ALUNO.LOCAL>
2025-08-08 09:26:43 -0300

    criando os metodos para o calculo do valor de cada parcela e finalizando o projeto

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const MESSAGE_ERROR_OUT_OF_RANGE = 'ERRO: os valores informados precisam ser aci
 
 //import da biblioteca readline
 var readline = require('readline')
+const { calcularMontante, calcularJurosCompostos } = require('./modulo/juros')
 
 //criando um objeto para entrada e saida de dados via terminal
 var entradaDeDados = readline.createInterface({
@@ -34,31 +35,46 @@ entradaDeDados.question('Digite o nome do cliente:', function (nome) {
 
             //entrada de dados do juros
             entradaDeDados.question('insira a taxa de juros anual:', function (valor2) {
-                let juros = valor2
+                let juros = valor2 / 100
 
                 //entrada de dados das parcelas
                 entradaDeDados.question('digite o numero de juros compostos por ano:', function (valor3) {
-                    let parcelas = valor3
+                    let periodos = valor3
 
                     //entrada de dados do tempo em anos
                     entradaDeDados.question('digite o tempo em anos:', function (valor4) {
                         let tempo = valor4
+                        
+                        //entrada de dados de numero de parcelas
+                        entradaDeDados.question('digite o numero de parcelas:', function(valor5) {
+                            let parcelas = valor5
 
-                        if (capital == '' || juros == '' || parcelas == '' || tempo == '' || nome == '') {
-                            console.log(MESSAGE_ERROR_EMPTY)
+                            if (capital == '' || juros == '' || periodos == '' || tempo == '' || nome == '' || parcelas == '') {
+                                console.log(MESSAGE_ERROR_EMPTY)
+    
+                            } else if (isNaN(capital) || isNaN(juros) || isNaN(periodos) || isNaN(tempo) || isNaN(parcelas)) {
+                                console.log(MESSAGE_ERROR_NOT_NUMBER)
+    
+                            } else if (Number(capital) < 0 || Number(juros) < 0 || Number(periodos) < 0 || Number(tempo) < 0 || Number(parcelas) < 0) {
+                                console.log(MESSAGE_ERROR_OUT_OF_RANGE)
+    
+                            } else {
+                                let montante = calcularMontante(valor1, valor2, valor3, valor4)
+                                let parcelaC = calcularJurosCompostos(montante, juros, valor5)
 
-                        } else if (isNaN(capital) || isNaN(juros) || isNaN(parcelas) || isNaN(tempo)) {
-                            console.log(MESSAGE_ERROR_NOT_NUMBER)
-
-                        } else if (Number(capital) < 0 || Number(juros) < 0 || Number(parcelas) < 0 || Number(tempo) < 0) {
-                            console.log(MESSAGE_ERROR_OUT_OF_RANGE)
-
-                        } else {
-                            
-                        }
-                    })
-                })
-            })
-        })
-    })
-})
+                                console.log(`******************* [Nome da Empresa] *******************
+Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
+A compra do produto ${nomeProduto}, tem um valor de: R$${capital}.
+A sua compra será parcelada em ${parcelas} vezes e o Sr(a) pagará no total: R$${montante} 
+com cada parcela saindo por: R$${parcelaC}
+Muito obrigado por escolher a [Nome da Empresa].
+*******************************************************`)
+                                entradaDeDados.close()
+                            }
+                        })//entrada de dados da parcela
+                    })//entrada de dados do tempo
+                })//entrada de dados do periodo
+            })//entrada de dados do juros
+        })//entrada de dados da capital
+    })//entrada de dados do nomeProduto
+})//entrada de dados do nome
--- modulo/juros.js
+++ modulo/juros.js
@@ -7,12 +7,28 @@
 
 function calcularMontante (valor1, valor2, valor3, valor4){
     let capital = valor1
-    let juros = valor2
-    let parcelas = valor3
+    let juros = valor2 / 100
+    let periodos = valor3
     let tempo = valor4
 
-    let montante = Number(capital) * (1 + Number(juros/100) / Number(parcelas)) ** (Number(parcelas) * Number(tempo))
+    let montante = Number(capital) * (1 + Number(juros) / Number(periodos)) ** (Number(periodos) * Number(tempo))
 
-    console.log(calcularMontante(100,50,6,1))
-    return Number(montante).toFixed(1)
+    return Number(montante).toFixed(2)
+}
+
+function calcularJurosCompostos (montante, juros, valor5){
+    let numeroParcelas = valor5
+
+    let fator = juros * (1 + juros) ** Number(numeroParcelas) / ((1 + juros) ** Number(numeroParcelas) - 1);
+    let parcelaC = Number(montante) * Number(fator);
+
+    return Number(parcelaC).toFixed(2)
+}
+
+//console.log(calcularMontante(100,50,6,1))
+//console.log(calcularJurosCompostos(123,0.05,5))
+
+module.exports = {
+    calcularMontante,
+    calcularJurosCompostos
 }
